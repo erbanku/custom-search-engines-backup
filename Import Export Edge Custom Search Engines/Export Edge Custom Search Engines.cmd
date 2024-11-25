4e2e49c5e94710756d19c2fe5f40d67c23d82513
erbanku <79493862+erbanku@users.noreply.github.com>
2024-11-25 14:06:07 +0800

    feat: attempt to support multiple profiles

--- Import Export Edge Custom Search Engines/Export Edge Custom Search Engines.cmd
+++ Import Export Edge Custom Search Engines/Export Edge Custom Search Engines.cmd
@@ -1,23 +1,83 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 set CURRENT_DRIVE=%~d0
 set CURRENT_DIR=%~p0
+set EDGE_PATH=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Microsoft\Edge\User Data
+set TEMP_SQL_SCRIPT=%TEMP%\sync_edge_sql_script
+set TEMP_DB=%TEMP%\temp_web_data
+
 if "%1"=="" (
-	set DESTINATION=%CURRENT_DRIVE%%CURRENT_DIR%Edgekeywords.sql
+    set BASE_DESTINATION=%CURRENT_DRIVE%%CURRENT_DIR%Edgekeywords
 ) else (
-	set DESTINATION=%~f1
+    set BASE_DESTINATION=%~f1
 )
 
-set DESTINATION=%DESTINATION:\=/%
-set TEMP_SQL_SCRIPT=%TEMP%\sync_edge_sql_script
-
 pushd
-echo Exporting Edge keywords to %DESTINATION%...
-cd /D "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Microsoft\Edge\User Data\Profile 1"
-echo .output "%DESTINATION%" > %TEMP_SQL_SCRIPT%
-echo .dump keywords >> %TEMP_SQL_SCRIPT%
-"%CURRENT_DRIVE%%CURRENT_DIR%\sqlite3.exe" -init %TEMP_SQL_SCRIPT% "Web Data" .exit
-del %TEMP_SQL_SCRIPT%
-popd
+echo WARNING: You should close all Edge instances before running this script.
+:: List available profiles
+set profile_count=0
+echo Available Edge profiles:
+for /d %%P in ("%EDGE_PATH%\*") do (
+    if exist "%%P\Web Data" (
+        set /a profile_count+=1
+        echo !profile_count!: %%~nxP
+    )
+)
 
-if errorlevel 1 pause
+:: Get user selection
+set /p "selection=Enter profile numbers to export (comma-separated) or 'all' for all profiles: "
+
+:: Process each profile
+set profile_num=0
+for /d %%P in ("%EDGE_PATH%\*") do (
+    if exist "%%P\Web Data" (
+        set /a profile_num+=1
+        set "export_profile="
+        
+        :: Check if this profile should be exported
+        if /i "!selection!"=="all" (
+            set "export_profile=yes"
+        ) else (
+            for %%n in (!selection!) do (
+                if %%n==!profile_num! set "export_profile=yes"
+            )
+        )
+        
+        if defined export_profile (
+            set "PROFILE_NAME=%%~nxP"
+            set DESTINATION=%BASE_DESTINATION%_!PROFILE_NAME!.sql
+            set DESTINATION=!DESTINATION:\=/!
+            
+            echo Exporting Edge keywords from !PROFILE_NAME! to !DESTINATION!...
+            cd /D "%%P"
+            
+            rem Copy database to temp location to avoid locks
+            copy /Y "Web Data" "%TEMP_DB%" >nul
+            
+            echo .output "!DESTINATION!" > %TEMP_SQL_SCRIPT%
+            echo .dump keywords >> %TEMP_SQL_SCRIPT%
+            "%CURRENT_DRIVE%%CURRENT_DIR%\sqlite3.exe" -init %TEMP_SQL_SCRIPT% "%TEMP_DB%" .exit
+            
+            if exist "!DESTINATION!" (
+                for %%F in ("!DESTINATION!") do if %%~zF gtr 0 (
+                    echo Successfully exported !PROFILE_NAME!
+                ) else (
+                    echo Failed to export !PROFILE_NAME! - empty file
+                    pause
+                )
+            ) else (
+                echo Failed to export !PROFILE_NAME! - file not created
+                pause
+            )
+            
+            rem Clean up temp files
+            del %TEMP_SQL_SCRIPT% 2>nul
+            del %TEMP_DB% 2>nul
+        )
+    )
+)
+
+popd
+echo Export complete.
+pause
--- Overwrite Edge Custom Search Engines to Chrome/Overwrite Edge Custom Search Engines to Chrome.cmd
+++ Overwrite Edge Custom Search Engines to Chrome/Overwrite Edge Custom Search Engines to Chrome.cmd
@@ -1,92 +1,114 @@
 @echo off
+setlocal EnableDelayedExpansion
+
+:: Welcome message
+echo WARNING: You should close all Edge and Chrome instances before running this script.
+echo This script will overwrite custom search engines in Chrome profiles with those exported ones from Edge profiles.
+echo.
 
 set CURRENT_DRIVE=%~d0
 set CURRENT_DIR=%~p0
-if "%1"=="" (
-	set DESTINATION=%CURRENT_DRIVE%%CURRENT_DIR%Edgekeywords.sql
-) else (
-	set DESTINATION=%~f1
-)
+set CHROME_PATH=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Google\Chrome\User Data
+set EDGE_PATH=%HOMEDRIVE%%HOMEPATH%\AppData\Local\Microsoft\Edge\User Data
+set TEMP_SQL_SCRIPT=%TEMP%\sync_sql_script
+set TEMP_DB=%TEMP%\temp_web_data
 
-set DESTINATION=%DESTINATION:\=/%
-set TEMP_SQL_SCRIPT=%TEMP%\sync_edge_sql_script
+if "%1"=="" (
+    set BASE_DESTINATION=%CURRENT_DRIVE%%CURRENT_DIR%Edgekeywords
+) else (
+    set BASE_DESTINATION=%~f1
+)
 
 pushd
-echo Exporting Edge keywords to %DESTINATION%...
-cd /D "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Microsoft\Edge\User Data\Profile 1"
-echo .output "%DESTINATION%" > %TEMP_SQL_SCRIPT%
+
+:: List available Edge profiles
+set edge_profile_count=0
+echo Available Edge profiles:
+for /d %%P in ("%EDGE_PATH%\*") do (
+    if exist "%%P\Web Data" (
+        set /a edge_profile_count+=1
+        echo !edge_profile_count!: %%~nxP
+        set "edge_profile_!edge_profile_count!=%%~nxP"
+    )
+)
+
+:: List available Chrome profiles
+set chrome_profile_count=0
+echo Available Chrome profiles:
+for /d %%P in ("%CHROME_PATH%\*") do (
+    if exist "%%P\Web Data" (
+        set /a chrome_profile_count+=1
+        echo !chrome_profile_count!: %%~nxP
+        set "chrome_profile_!chrome_profile_count!=%%~nxP"
+    )
+)
+
+:: Get user selection for Edge and Chrome profile pairs
+:select_profiles
+set /p "edge_selection=Enter Edge profile number to export (or 'done' to finish after overwrite complete): "
+if /i "!edge_selection!"=="done" goto end
+set /p "chrome_selection=Enter Chrome profile number to import to: "
+
+:: Process the selected profiles
+set "edge_profile=!edge_profile_%edge_selection%!"
+set "chrome_profile=!chrome_profile_%chrome_selection%!"
+
+set DESTINATION=%BASE_DESTINATION%_!edge_profile!.sql
+set DESTINATION=!DESTINATION:\=/!
+
+echo Exporting Edge keywords from !edge_profile! to !DESTINATION!...
+cd /D "%EDGE_PATH%\!edge_profile!"
+
+rem Copy database to temp location to avoid locks
+copy /Y "Web Data" "%TEMP_DB%" >nul
+
+echo .output "!DESTINATION!" > %TEMP_SQL_SCRIPT%
 echo .dump keywords >> %TEMP_SQL_SCRIPT%
-"%CURRENT_DRIVE%%CURRENT_DIR%\sqlite3.exe" -init %TEMP_SQL_SCRIPT% "Web Data" .exit
-del %TEMP_SQL_SCRIPT%
-popd
+"%CURRENT_DRIVE%%CURRENT_DIR%\sqlite3.exe" -init %TEMP_SQL_SCRIPT% "%TEMP_DB%" .exit
 
-if errorlevel 1 pause
-
-
-@echo off
-
-set CURRENT_DRIVE=%~d0
-set CURRENT_DIR=%~p0
-if "%1"=="" (
-	set DESTINATION=%CURRENT_DRIVE%%CURRENT_DIR%Chromekeywordsbackup.sql
+if exist "!DESTINATION!" (
+    for %%F in ("!DESTINATION!") do if %%~zF gtr 0 (
+        echo Successfully exported !edge_profile!
+    ) else (
+        echo Failed to export !edge_profile! - empty file
+        pause
+    )
 ) else (
-	set DESTINATION=%~f1
+    echo Failed to export !edge_profile! - file not created
+    pause
 )
 
-set DESTINATION=%DESTINATION:\=/%
-set TEMP_SQL_SCRIPT=%TEMP%\sync_chrome_sql_script
+rem Clean up temp files
+del %TEMP_SQL_SCRIPT% 2>nul
+del %TEMP_DB% 2>nul
 
-pushd
-echo Exporting Chrome keywords to %DESTINATION%...
-cd /D "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Google\Chrome\User Data\Default"
-echo .output "%DESTINATION%" > %TEMP_SQL_SCRIPT%
-echo .dump keywords >> %TEMP_SQL_SCRIPT%
-"%CURRENT_DRIVE%%CURRENT_DIR%\sqlite3.exe" -init %TEMP_SQL_SCRIPT% "Web Data" .exit
-del %TEMP_SQL_SCRIPT%
+set SOURCE=%BASE_DESTINATION%_!edge_profile!.sql
+set SOURCE=!SOURCE:\=/!
+
+if exist "!SOURCE!" (
+    echo Importing Chrome keywords to !chrome_profile! from !SOURCE!...
+    cd /D "%CHROME_PATH%\!chrome_profile!"
+    
+    rem Copy database to temp location to avoid locks
+    copy /Y "Web Data" "%TEMP_DB%" >nul
+    
+    "%CURRENT_DRIVE%%CURRENT_DIR%\sqlite3.exe" "%TEMP_DB%" < "!SOURCE!"
+    
+    rem Replace the original database with the updated one
+    copy /Y "%TEMP_DB%" "Web Data" >nul
+    
+    echo Successfully imported keywords to !chrome_profile!
+    
+    rem Clean up temp files
+    del %TEMP_DB% 2>nul
+) else (
+    echo Source file for !chrome_profile! not found: !SOURCE!
+    pause
+)
+
+goto select_profiles
+
+:end
 popd
-
-if errorlevel 1 pause
-
-
-@echo off
-
-tasklist /FI "IMAGENAME eq chrome.exe" 2>NUL | find /I /N "chrome.exe">NUL
-if "%ERRORLEVEL%"=="0" (
-	echo Close Chrome and try again...
-	pause
-	exit /B 1
-)
-
-set /P PROCEED=This will overwrite your Google Chrome search engines! Are you sure?  
-if "%PROCEED%"=="Y" goto DoIt
-if "%PROCEED%"=="y" goto DoIt
-if "%PROCEED%"=="YES" goto DoIt
-if "%PROCEED%"=="Yes" goto DoIt
-if "%PROCEED%"=="yes" goto DoIt
-echo Cancelled operation.
+echo Operation complete.
 pause
-exit /B 2
-
-:DoIt
-set CURRENT_DRIVE=%~d0
-set CURRENT_DIR=%~p0
-if "%1"=="" (
-	set SOURCE=%CURRENT_DRIVE%%CURRENT_DIR%Edgekeywords.sql
-) else (
-	set SOURCE=%~f1
-)
-
-set SOURCE=%SOURCE:\=/%
-set TEMP_SQL_SCRIPT=%TEMP%\sync_chrome_sql_script
-
-pushd
-echo Importing Edge keywords from %SOURCE%...
-cd /D "%HOMEDRIVE%%HOMEPATH%\AppData\Local\Google\Chrome\User Data\Default"
-echo DROP TABLE IF EXISTS keywords;> %TEMP_SQL_SCRIPT%
-echo .read "%SOURCE%">> %TEMP_SQL_SCRIPT%
-copy "Web Data" "Web Data.backup"
-"%CURRENT_DRIVE%%CURRENT_DIR%\sqlite3.exe" -init %TEMP_SQL_SCRIPT% "Web Data" .exit
-del %TEMP_SQL_SCRIPT%
-popd
-
-if errorlevel 1 pause
